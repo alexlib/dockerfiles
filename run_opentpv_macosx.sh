3db8d53c70cf06b9cd577ed46e70e10d0ce2ac71
Alex Liberzon <alexlib@users.noreply.github.com>
2017-04-16 01:41:22 +0300

    Update run_opentpv_macosx.sh

--- run_opentpv_macosx.sh
+++ run_opentpv_macosx.sh
@@ -1,5 +1,6 @@
+docker build -t openptv .
+
 open -a XQuartz
 IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 xhost + $IP
-docker build -t openptv .
 docker run --rm -it --name openptv -e DISPLAY=$IP:0 -v /tmp/.X11-unix:/tmp/.X11-unix openptv
