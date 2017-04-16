368c75ae8c3582c8df4478bf7c7a2b4b829c9fe3
Alex Liberzon <alexlib@users.noreply.github.com>
2017-04-16 21:07:47 +0300

    Update run_opentpv_macosx.sh
    thanks to this website http://unitstep.net/blog/2015/12/21/accessing-the-host-filesystem-from-a-docker-container-on-os-x-or-windows/ we can now copy things from the container to the host system and back, on Mac and Windows (not tested yet)

--- run_opentpv_macosx.sh
+++ run_opentpv_macosx.sh
@@ -3,4 +3,8 @@ docker build -t openptv .
 open -a XQuartz
 IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 xhost + $IP
-docker run --rm -it --name openptv -e DISPLAY=$IP:0 -v /tmp/.X11-unix:/tmp/.X11-unix openptv
+docker run -it --name=openptv \
+        -e DISPLAY=$IP:0 \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        -v /Users:/host/Users \
+openptv
